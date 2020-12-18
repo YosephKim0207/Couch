947a223bc5153aa10138c4c9ec6ee193b1a6cfab
Yoseph Kim <46564046+YosephKim0207@users.noreply.github.com>
2020-12-18 23:51:38 +0900

    django-filebrowser code

---
+++ urls.py
@@ -0,0 +1,46 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from filebrowser.sites import site
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+#from . import views
+#from django.conf.urls import url
+
+#urlpatterns = patterns('',
+#   url(r'^adminurl/filebrowser/', include(site.urls)),
+#)
+
+urlpatterns = [
+    url(r'^adminurl/filebrowser/', site.urls),
+    path('admin/filebrowser/', site.urls),
+    path('grappelli/', include('grappelli.urls')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG: # will be 'True' in development
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.core.files.storage import DefaultStorage
+from filebrowser.sites import FileBrowserSite
+
+# Default FileBrowser site
+site = FileBrowserSite(name='filebrowser', storage=DefaultStorage())
